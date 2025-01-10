059e3812966ec4743ae8d150d2e850f93e5938df
Todd Taylor <150555214+tt01924@users.noreply.github.com>
2025-01-10 14:23:57 +0000

    calculator design

--- src/App.js
+++ src/App.js
@@ -4,20 +4,48 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+      <div className="calculator">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>
+          Lloyds Mortgage Calculator
+        </h1>
+
+        <div className="row">
+          <p>Property price:</p>
+          <input type="text" className="input" placeholder='£' />
+        </div>
+
+        <div className="row">
+          <p>Income:</p>
+          <input type="text" className="input" placeholder='Per year'/>
+        </div>
+
+        <div className="row">
+          <p>Deposit:</p>
+          <input type="text" className="input" placeholder='£'/>
+        </div>
+
+        <div className="row">
+          <p>Repayment period:</p>
+          <input type="text" className="input" placeholder='No. of years'/>
+        </div>
+
+        <div className="row">
+          <p>Average interest rate:</p>
+          <input type="text" className="input" placeholder='%'/>
+        </div>
+
+        <div className="row">
+          <p>Est. monthly repayments:</p>
+          <input type="text" className="input" placeholder='£'/>
+        </div>
+
+        <div className="row">
+          <p>Est. borrow amount:</p>
+          <input type="text" className="input" placeholder='£'/>
+        </div>
+      </div>
     </div>
   );
 }
