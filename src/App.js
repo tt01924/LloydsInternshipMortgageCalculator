864c684a4a9c1043183e03276dddab227039fee6
Todd Taylor <150555214+tt01924@users.noreply.github.com>
2025-01-15 22:47:05 +0000

    adding in rates for different loan types

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [loanReason, setLoanReason] = useState();
+  const [interestRate, setInterestRate] = useState();
+
+  const handleLoanReasonChange = (event) => {
+    const selectedReason = event.target.value;
+    setLoanReason(selectedReason);
+
+    // Determine interest rate based on loan reason
+    switch (selectedReason) {
+      case 'Basic Mortgage':
+        setInterestRate('3.5%');
+        break;
+      case 'Interest-Only Mortgage':
+        setInterestRate('2.8%');
+        break;
+      case 'Large Loan Amount':
+        setInterestRate('4.2%');
+        break;
+      case 'Short Term Loan':
+        setInterestRate('3.0%');
+        break;
+      case 'High Interest Rate':
+        setInterestRate('5.5%');
+        break;
+      default:
+        setInterestRate(''); // Clear if no valid option is selected
+    }
+  };
+
   return (
     <div className="App">
 
@@ -10,6 +40,18 @@ function App() {
         <h1>
           Lloyds Mortgage Calculator
         </h1>
+
+        <div className="row">
+          <p>Reason for loan:</p>
+          <select onChange={handleLoanReasonChange}>
+            <option value=''>Please select an option</option>
+            <option>Basic Mortgage</option>
+            <option>Interest-Only Mortgage</option>
+            <option>Large Loan Amount</option>
+            <option>Short Term Loan</option>
+            <option>High Interest Rate</option>
+          </select>
+        </div>
 
         <div className="row">
           <p>Property price:</p>
@@ -33,7 +75,7 @@ function App() {
 
         <div className="row">
           <p>Average interest rate:</p>
-          <input type="text" className="input" placeholder='%'/>
+  <p>{interestRate || 'Please select a loan reason'}</p>
         </div>
 
         <div className="row">
